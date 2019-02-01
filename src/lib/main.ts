996531c4fd9136b82a76c805080cfc979f6839e6
Soutaro Matsumoto <matsumoto@soutaro.com>
2019-02-01 16:30:54 +0900

    Handling options

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -4,6 +4,18 @@ import * as fs from "fs"
 import {promisify} from 'util'
 import * as path from 'path'
 import ConfigFile from "./model/ConfigFile";
+import yargs from "yargs";
+
+const args = yargs.usage("Usage: $0 [options] <destination>")
+  .describe("baseurl", "URL to access Sider (example: http://localhost:3000)")
+  .describe("mysql", "MySQL host (example: user:password@host:port)")
+  .describe("redis", "Redis host (example: host:port)")
+  .describe("catpost", "catpost endpoint (example: catpost.local)")
+  .describe("setaria", "setaria endpoint (example: setaria.local)")
+  .describe("minio", "Minio configuration (example: endpoint:key-id:secret-access-key:region:bucket")
+  .demandCommand(1)
+
+const argv = args.argv
 
 const mkdir = promisify(fs.mkdir)
 const writeFile = promisify(fs.writeFile)
@@ -14,11 +26,59 @@ async function writeConfigFile(dir: string, file: ConfigFile) {
   return writeFile(p, file.toString())
 }
 
-async function main(argv: string[]) {
-  const config: Configuration = generateRandomConfiguration()
+async function main() {
+  let config: Configuration = generateRandomConfiguration()
+  if (argv.mysql) {
+    config = { ...config, mysqlHost: argv.mysql as string }
+  }
+  if (argv.redis) {
+    config = { ...config, redisHost: argv.redis as string }
+  }
+  if (argv.baseurl) {
+    config = { ...config, endpoint: argv.baseurl as string }
+  }
+  if (argv.catpost) {
+    config = {
+      ...config,
+      catpost: {
+        ...config.catpost,
+        endpoint: argv.catpost as string
+      }
+    }
+  }
+  if (argv.setaria) {
+    config = {
+      ...config,
+      setaria: {
+        ...config.setaria,
+        endpoint: argv.setaria as string
+      }
+    }
+  }
+  if (argv.minio) {
+    const components = (argv.minio as string).split(":")
+    if (components.length != 5) {
+      console.error("--minio should be colon separated 5 items of string.")
+      return
+    }
+    config = {
+      ...config,
+      catpost: {
+        ...config.catpost,
+        minio: {
+          endpoint: components[0],
+          accessKeyId: components[1],
+          secretAccessKey: components[2],
+          regionName: components[3],
+          bucketName: components[4]
+        }
+      }
+    }
+  }
+
   const { email, sideci, catpost, setaria } = generate(config)
   
-  const dir = path.join(process.cwd(), argv[2] || ".")
+  const dir = path.join(process.cwd(), argv._[0] || ".")
   
   if (!fs.existsSync(dir)) {
     console.info(`Making dir: ${dir}...`)
@@ -31,4 +91,4 @@ async function main(argv: string[]) {
   await writeConfigFile(dir, setaria)
 }
 
-main(process.argv)
+main()
